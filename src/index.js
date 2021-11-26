c0b7351ee5a6ccf5c929bbf5cc0bd879e7961172
Sarah <sarahlchavez@gmail.com>
2021-11-26 12:15:21 -0800

    commit tree b90e092cf267ba5b66ccd93cdf502d026be062cb

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 // write your createStore function here
+// Write a function called createStore that 
+// takes in a reducer function as an argument.
+// The createStore function should return an 
+// object with two methods - getState, dispatch.
+// get state should return the current state
+// dispatch takes in an action, updates the state 
+// using the reducer
+
+function createStore(candyReducer) {
+  let state;
+
+  function dispatch(action) {
+    state = candyReducer(state, action)
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return {
+    dispatch,
+    getState
+  }
+}
 
 function candyReducer(state = [], action) {
   switch (action.type) {
@@ -9,6 +33,15 @@ function candyReducer(state = [], action) {
   }
 }
 
+// define a variable named store
+// and assign it to whatever is returned
+// by createStore function when it is passed the
+// candyReducer function
+
+let store = createStore(candyReducer)
+store.dispatch({ type: '@@INIT' })
+
+
 function render() {
   let container = document.getElementById('container');
   if(store.getState()) {
@@ -18,5 +51,6 @@ function render() {
   }
 };
 
-// Use your createStore function and the functions provided here to create a store.
+// Use your createStore function and the functions provided 
+// here to create a store.
 // Once the store is created, call an initial dispatch.
